b2cc9b3de3ab683d781b418fc01650d9ed17231e
Bohdan Moskalenko <b.v.moskalenko@gmail.com>
2019-01-26 01:58:26 +0200

    Changes after reading of documentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,36 @@
-var gulp = require('gulp');
-var webserver = require('gulp-webserver');
+const config = require('./app.config.json')
 
-gulp.task('webserver', function() {
-  gulp.src('public')
-    .pipe(webserver({
-      livereload: true,
-      directoryListing: true
-    }))
-})
+const plugins = {
+  gulp: require('gulp'),
+  browserSync: require('browser-sync'),
+  shell: require('gulp-shell')
+}
+
+const tasks = {
+  rebuildPatternLab: () => (
+    plugins.gulp.src('./', { read: false })
+    .pipe(plugins.shell([
+      'php core/console --generate'
+    ]))
+    .pipe(plugins.browserSync.reload({ stream: true }))
+  ),
+  copyStyleGuide: () => (
+    plugins.gulp.src(config.patternlab.styleguide.files)
+      .pipe(plugins.gulp.dest(config.patternlab.styleguide.dest))
+  ),
+  browserSync: () => (
+    plugins.browserSync({
+      server: {
+        baseDir: config.root,
+      },
+      ghostMode: true
+    })
+  )
+}
+
+const watch = () => {
+  tasks.browserSync()
+  plugins.gulp.watch(config.patternlab.files, tasks.rebuildPatternLab)
+}
+
+plugins.gulp.task('dev', plugins.gulp.series(tasks.rebuildPatternLab, tasks.copyStyleGuide, watch))
