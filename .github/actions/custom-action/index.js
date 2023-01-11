17e4cef23575e6806316bafe7acc25a479a74008
Bohdan Moskalenko <b.v.moskalenko@gmail.com>
2023-01-12 01:28:28 +0200

    Test

--- .github/actions/custom-action/index.js
+++ .github/actions/custom-action/index.js
@@ -1,8 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  core.setOutput('data', 'someData');
-} catch (error) {
-  core.setFailed(error.message);
-}
+const run = async () => {
+  try {
+    const octokit = github.getOctokit(
+      core.getInput('token', { required: true })
+    );
+
+    const data = await octokit.request(
+      'GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews',
+      {
+        owner: github.getOctokit(core.getInput('owner')),
+        repo: github.getOctokit(core.getInput('repo')),
+      }
+    );
+
+    core.setOutput('data', 'someData');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+run();
