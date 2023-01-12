eafce6bb0369ce18363a68a9fcac58d9e82db53c
Bohdan Moskalenko <b.v.moskalenko@gmail.com>
2023-01-12 12:21:52 +0200

    Test

--- .github/actions/custom-action/index.js
+++ .github/actions/custom-action/index.js
@@ -10,7 +10,7 @@ const run = async () => {
     const nwo = process.env.GITHUB_REPOSITORY;
     const [owner, repo] = nwo.split('/');
 
-    console.log(github);
+    console.log(process.env);
 
     // const data = await octokit.request(
     //   'GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews',
