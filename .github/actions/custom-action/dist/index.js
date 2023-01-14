09ca28fb55855d8d026b2f770c02b62cece21321
Bohdan Moskalenko <b.v.moskalenko@gmail.com>
2023-01-14 16:25:01 +0200

    Test

--- .github/actions/custom-action/dist/index.js
+++ .github/actions/custom-action/dist/index.js
@@ -10684,43 +10684,45 @@ async function run() {
       auth: token,
     });
 
-    const res = await octokit.request(
-      `GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews?per_page=100`,
-      {
-        owner: owner,
-        repo: repo,
-        pull_number: github.context.payload.pull_request.number,
-      }
-    );
+    console.log(github);
 
-    if (!res.data.length) {
-      core.setOutput('data', false);
-    } else {
-      const uniqueUserApproves = res.data.reduce((acc, item) => {
-        if (acc[item.user.id] || item.state.toLowerCase() !== 'approved')
-          return acc;
+    // const res = await octokit.request(
+    //   `GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews?per_page=100`,
+    //   {
+    //     owner: owner,
+    //     repo: repo,
+    //     pull_number: github.context.payload.pull_request.number,
+    //   }
+    // );
 
-        acc[item.user.id] = true;
+    // if (!res.data.length) {
+    //   core.setOutput('data', false);
+    // } else {
+    //   const uniqueUserApproves = res.data.reduce((acc, item) => {
+    //     if (acc[item.user.id] || item.state.toLowerCase() !== 'approved')
+    //       return acc;
 
-        return acc;
-      }, {});
+    //     acc[item.user.id] = true;
 
-      const uniqueApproves = Object.keys(uniqueUserApproves).length;
+    //     return acc;
+    //   }, {});
 
-      if (
-        github.context.eventName === 'pull_request' &&
-        uniqueApproves >= approveCount
-      ) {
-        core.setOutput('data', true);
-      } else if (
-        github.context.eventName === 'pull_request_review' &&
-        uniqueApproves == approveCount
-      ) {
-        core.setOutput('data', true);
-      } else {
-        core.setOutput('data', false);
-      }
-    }
+    //   const uniqueApproves = Object.keys(uniqueUserApproves).length;
+
+    //   if (
+    //     github.context.eventName === 'pull_request' &&
+    //     uniqueApproves >= approveCount
+    //   ) {
+    //     core.setOutput('data', true);
+    //   } else if (
+    //     github.context.eventName === 'pull_request_review' &&
+    //     uniqueApproves == approveCount
+    //   ) {
+    //     core.setOutput('data', true);
+    //   } else {
+    //     core.setOutput('data', false);
+    //   }
+    // }
   } catch (error) {
     core.setFailed(error.message);
   }
