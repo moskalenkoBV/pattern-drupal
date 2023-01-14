bd92d4d7853daa87e774b06c1924d030ab28376e
Bohdan Moskalenko <b.v.moskalenko@gmail.com>
2023-01-14 18:01:57 +0200

    Test

--- .github/actions/custom-action/index.js
+++ .github/actions/custom-action/index.js
@@ -28,6 +28,7 @@ async function run() {
       repo: repo,
       head_sha: github.context.payload.pull_request.head.sha,
       name: 'code-coverage',
+      status: 'completed',
     });
 
     // const res = await octokit.request(
